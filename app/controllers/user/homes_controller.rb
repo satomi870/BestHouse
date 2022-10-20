8600f93e61d0b36d9e912b43ae9f471f302dc1e2
EC2 Default User <ec2-user@ip-172-31-11-23.us-west-1.compute.internal>
2022-10-20 12:07:54 +0000

    [add]物件詳細ページ

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,5 +1,7 @@
 class User::HomesController < ApplicationController
   def top
+    @areas=Area.all
+    @tags=Tag.all
   end
 
   def about
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,23 +19,26 @@ Area.create!(
   area_name:'世田谷区'
   )
 
-Property.create!(
+property=Property.create!(
     area_id: 1, property_name:'物件A', address:'東京都', access:'中村橋駅 徒歩3分', rent:40000, condition:'女性', vacancy:'個室6',introduction:'主要駅までのアクセス良好です', common_service_fee:15000,
     room_facility:'エアコン・ベッド、クローゼット',
     living:'テレビ、ソファ、テーブル', kitchen:'電子レンジ、冷蔵庫、炊飯器', bath:'シャワールーム 3室',toilet:'洋式2室',others:'洗濯機 2台')
+property.image.attach(io: File.open(Rails.root.join('app/assets/images/house1.jpg')),filename: 'house1.jpg')
     #profile_image: File.open("./app/assets/images/house1.jpg"))
-    #property.avatar.attach(io: File.open(Rails.root.join('app/assets/images/logo.jpg')),
+    #property.avatar.attach(io: File.open(Rails.root.join('app/assets/images/house1.jpg')),
                   #filename: 'logo.jpg')
-Property.create!(
+property=Property.create!(
     area_id: 2,property_name:'物件B', address:'東京都', access:'池袋駅 徒歩7分', rent:50000, condition:'男女どちらともOK', vacancy:'個室4', introduction:'主要駅までのアクセス良好です', common_service_fee:18000,
     room_facility:'エアコン・ベッド・クローゼット、TV',
     living:'テレビ、ソファ、テーブル、エアコン', kitchen:'電子レンジ,冷蔵庫,炊飯器、トースター', bath:'シャワールーム 4室',toilet:'洋式3室',others:'洗濯機 3台')
+property.image.attach(io: File.open(Rails.root.join('app/assets/images/house2.jpg')),filename: 'house2.jpg')
      #property.avatar.attach(io: File.open(Rails.root.join('app/assets/images/logo.jpg')),
                   #filename: 'logo.jpg')
-Property.create!(
+property=Property.create!(
     area_id: 3,property_name:'物件C', address:'東京都', access:'世田谷駅 徒歩9分', rent:40000, condition:'女性', vacancy:'個室6', introduction:'主要駅までのアクセス良好です', common_service_fee:15000,
     room_facility:'エアコン・ベッド・クローゼット、ミニ冷蔵庫',
     living:'テレビ、ソファ、テーブル、DVD', kitchen:'電子レンジ,冷蔵庫,炊飯器', bath:'シャワールーム 2室',toilet:'洋式3室',others:'洗濯機 2台')
+property.image.attach(io: File.open(Rails.root.join('app/assets/images/house3.jpg')),filename: 'house3.jpg')
     #property.avatar.attach(io: File.open(Rails.root.join('app/assets/images/logo.jpg')),
                   #filename: 'logo.jpg')
 
