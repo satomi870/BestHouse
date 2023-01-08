736ec12ca6d9d955e516848c072f2ca56f5e3957
EC2 Default User <ec2-user@ip-172-31-3-209.ap-northeast-1.compute.internal>
2023-01-08 05:31:28 +0000

    remove migration

--- db/migrate/20221224064400_remove_propertyid_to_notifications.rb
+++
@@ -1,4 +0,0 @@
-class RemovePropertyidToNotifications < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/migrate/20221224064641_remove_propertyid_from_notifications.rb
+++
@@ -1,5 +0,0 @@
-class RemovePropertyidFromNotifications < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :notifications, :propertyid, :string
-  end
-end
