e31802cffb96ac2b52ba934af702ac6f0d22f11b
EC2 Default User <ec2-user@ip-172-31-3-209.ap-northeast-1.compute.internal>
2023-01-11 10:23:04 +0000

    [fix]ログインエラー

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,36 +1,24 @@
 # frozen_string_literal: true
-
 class User::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
   before_action:user_deleted,only:[:create]
-  before_action:user_active,only:[:create]
-
 
   def user_deleted
     @user=User.find_by(email: params[:user][:email])
     return if !@user
-    if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
-    redirect_to new_user_registration_path
-    end
+      if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
+        redirect_to new_user_registration_path
+      end
   end
 
-  def user_active
-    @user=User.find_by(email: params[:user][:email])
-    return if !@user
-    if @user.valid_password?(params[:user][:password]) && (@user.is_active == true)
-    redirect_to new_user_registration_path
-    end
-
-  end
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource)
-    # root_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   stored_location_for(resource)
+  #   # root_path
+  # end
 
 
   def after_sign_in_path_for(resource)
     root_path
+  end
   # end
   # GET /resource/sign_in
   # def new
@@ -51,7 +39,5 @@ class User::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-end
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute
 end
