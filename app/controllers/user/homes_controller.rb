d60b47720abe716998b9201d6504a5df10a5fbe5
EC2 Default User <ec2-user@ip-172-31-3-209.ap-northeast-1.compute.internal>
2023-01-13 07:56:05 +0000

    [fix]ランキング

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -21,13 +21,13 @@ class User::HomesController < ApplicationController
 
     #ラジオボタンレビュー項目の平均点を物件ごとに算出して上位5件を表示
     review_with_atmosphere_avg = Review.group(:property_id).order("avg(atmosphere) desc").select('property_id, avg(atmosphere) as atmosphere_avg')
-    @properties_atmosphere = Property.find(review_with_atmosphere_avg.select { |review| review.atmosphere_avg >= 3.0 }.pluck(:property_id))
+    @properties_atmosphere = Property.find(review_with_atmosphere_avg.select { |review| review.atmosphere_avg >= 3.0 }.pluck(:property_id)).first(5)
     review_with_noise_avg = Review.group(:property_id).order("avg(noise) desc").select('property_id, avg(noise) as noise_avg')
-    @properties_noise = Property.find(review_with_noise_avg.select { |review| review.noise_avg >= 3.0 }.pluck(:property_id))
+    @properties_noise = Property.find(review_with_noise_avg.select { |review| review.noise_avg >= 3.0 }.pluck(:property_id)).first(5)
     review_with_cleanliness_shared_avg = Review.group(:property_id).order("avg(cleanliness_shared) desc").select('property_id, avg(cleanliness_shared) as cleanliness_shared_avg')
-    @properties_cleanliness_shared = Property.find(review_with_cleanliness_shared_avg.select { |review| review.cleanliness_shared_avg >= 3.0 }.pluck(:property_id))
+    @properties_cleanliness_shared = Property.find(review_with_cleanliness_shared_avg.select { |review| review.cleanliness_shared_avg >= 3.0 }.pluck(:property_id)).first(5)
     review_with_congestion_shared_avg = Review.group(:property_id).order("avg(congestion_shared) desc").select('property_id, avg(congestion_shared) as congestion_shared_avg')
-    @properties_congestion_shared = Property.find(review_with_congestion_shared_avg.select { |review| review.congestion_shared_avg >= 3.0 }.pluck(:property_id))
+    @properties_congestion_shared = Property.find(review_with_congestion_shared_avg.select { |review| review.congestion_shared_avg >= 3.0 }.pluck(:property_id)).first(5)
     #各物件のお気入り数が多い物件上位5件を表示
     @properties_favorites = Property.all.sort { |x, y| y.favorites.count <=> x.favorites.count }.reject { _1.favorites.count == 0 }.first(5)
     #review_with_atmo_avg = Review.group(:property_id).order("avg(atmosphere) desc").select('property_id, avg(atmosphere) as atmo_avg')
